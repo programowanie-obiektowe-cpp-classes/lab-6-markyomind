d3bc9d71c94fa866ff154b70b1d1f4ef6c73ef34
Marek Sadło <sadlo.marek.pg2@gmail.com>
2024-12-01 23:10:11 +0100

    test

--- foo.hpp
+++ foo.hpp
@@ -10,7 +10,11 @@ std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > retval(people.size());
 
-    // Twoja implementacja tutaj
+    std::for_each(people.begin(), people.end(), [](Human& person) { person.birthday(); });
+    std::transform(people.rbegin(), people.rend(), retval.begin(), [](const Human& person) 
+    {
+        return person.isMonster() ? 'n' : 'y';
+    });
 
     return retval;
 }
